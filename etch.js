5eecd3ed98c79d52cc2bcd23f16c4480a5939ccd
Revolution5 <damonboone99@gmail.com>
2022-04-03 11:43:45 -0700

    Add hover event listener for drawing

--- etch.js
+++ etch.js
@@ -4,10 +4,18 @@ function createGrid(number) {
     container.style.gridTemplateColumns = "repeat(" + number + ", 1fr)";
     for (let i = 0; i < (number * number); i++) {
         const square = document.createElement("div");
-        square.style.border = "solid 1px black"
+        square.classList.add("square")
+        // square.style.border = "solid 1px black"
         square.style.boxSizing = "border-box";
         container.appendChild(square);
     }
 }
-
 createGrid(16);
+
+const squares = Array.from(document.querySelectorAll(".square"));
+
+squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    })
+})
